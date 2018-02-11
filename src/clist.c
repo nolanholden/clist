a6e77ca64886cb93312093c6570e2599a24780c0
Nolan Holden <nolanholden@users.noreply.github.com>
2018-02-11 15:58:14 -0500

    progress

--- src/clist.c
+++ src/clist.c
@@ -22,6 +22,21 @@ struct cl_t {
   cl_node_t* tail;
 };
 
+void cl_swap_left(cl_t* const l, cl_node_t* const n) {
+  if (l == NULL || n == NULL || n->prev == NULL) return;
+  cl_node_t* const left = n->prev;
+  cl_node_t* const lmargin = n->prev->prev;
+  cl_node_t* const rmargin = n->next;
+
+  if (l->head == left) l->head = n;
+  if (l->tail == n)    l->tail = left;
+
+  n->prev = lmargin;
+  n->next = left;
+  left->prev = n;
+  left->next = rmargin;
+}
+
 cl_t* cl_alloc_list() {
   cl_t* list = (cl_t*)malloc(sizeof(cl_t));
   failif(!list);
@@ -32,110 +47,126 @@ cl_t* cl_alloc_list() {
 }
 
 size_t cl_size(const cl_t* l) { return l->count; }
+cl_node_t* cl_head(const cl_t* l) { return l->head; }
+cl_node_t* cl_tail(const cl_t* l) { return l->tail; }
 
-// cl_node_t* cl_alloc_node(void* data) {
-//   cl_node_t* node = (cl_node_t*)malloc(sizeof(cl_node_t));
-//   failif(!node);
 
-//   node->next  = NULL;
-//   node->prev = NULL;
-//   node->data = data;
-//   return node;
-// }
+cl_node_t* cl_alloc_node(void* data) {
+  cl_node_t* node = (cl_node_t*)malloc(sizeof(cl_node_t));
+  failif(!node);
 
-// const cl_node_t* cl_head(const cl_t* l) { return l->head; }
-// const cl_node_t* ccl_tail(const cl_t* l) { return l->tail; }
+  node->next  = NULL;
+  node->prev = NULL;
+  node->data = data;
+  return node;
+}
 
-// const cl_node_t* cl_next(const cl_node_t* const n) { return n->next; }
-// const cl_node_t* cl_prev(const cl_node_t* const n) { return n->prev; }
-// void* cl_data(const cl_node_t* const n) { return n->data; }
+const cl_node_t* cl_next(const cl_node_t* const n) { return n->next; }
+const cl_node_t* cl_prev(const cl_node_t* const n) { return n->prev; }
+void* cl_data(const cl_node_t* const n) { return n->data; }
+void cl_destroy(cl_t* const l) {
+  if (l == NULL) return;
+
+  for (cl_node_t* n = l->head; n != NULL;) {
+    free(n->data);
+    cl_node_t* tmp = n;
+    n = n->next;
+    free(tmp);
+  }
+
+  free(l);
+}
+
+void cl_destroy_node(cl_node_t* const n) {
+  if (n == NULL) return;
+  free(n->data);
+  free(n);
+}
+
+
+// Push a node to the front. If the list is empty, node will become head and
+// tail.
+void cl_push_front(cl_t* const l, cl_node_t* const n) {
+  if (l == NULL || n == NULL) return;
+  if (cl_is_empty(l)) {
+    n->next = n->prev = NULL;
+    l->head = l->tail = n;
+    l->count = 1;
+    return;
+  }
+  
+  n->prev = NULL;
+  n->next = l->head;
+  l->head->prev = n;
+  l->head = n;
+  l->count++;
+}
+
+void cl_push_back(cl_t* const l, cl_node_t* const n) {
+  if (l == NULL || n == NULL) return;
+  if (cl_is_empty(l)) {
+    n->next = n->prev = NULL;
+    l->head = l->tail = n;
+    l->count = 1;
+    return;
+  }
+
+  n->next = NULL;
+  n->prev = l->tail;
+  l->tail->next = n;
+  l->tail = n;
+  l->count++;
+}
+
+cl_node_t* cl_pop_back(cl_t* const l) {
+  if (l == NULL || l->tail == NULL) return NULL;
+
+  cl_node_t* const back = l->tail;
+  cl_node_t* const left = back->prev;
+
+  l->tail = left;
+  left->next = NULL;
+
+  back->next = back->prev = NULL;
+  l->count--;
+  return back;
+}
+
+void cl_pop_front(cl_t* const l) {
+  if (l == NULL || l->tail == NULL) return NULL;
+
+  cl_node_t* const front = l->tail;
+  cl_node_t* const right = right->next;
+
+  l->head = right;
+  right->prev = NULL;
+  
+  front->next = front->prev = NULL;
+  l->count--;
+  return front;
+}
 
 // // Return whether the list is empty. Always O(1).
-// int cl_is_empty(const cl_t* const l) { return l->head == NULL && l->tail == NULL; }
+int cl_is_empty(const cl_t* const l) { return l->head == NULL || l->tail == NULL; }
 
-// // Assuming first and second are connected nodes, insert the new node, 
-// // updating the pointers to reflect the change.
-// void cl_insert_between(cl_node_t* const new_node, cl_node_t* const first, cl_node_t* const second) {
-//   first->next = new_node;
-//   new_node->prev = first;
+void cl_sort(const cl_t* l,
+    int(*greater_than)(const cl_node_t* first, const cl_node_t* second)) {
+  if (l->count <= 1) return;
 
-//   second->prev = new_node;
-//   new_node->next = second;
-// }
+  for (cl_node_t* a = l->head->next; a != l->tail; a = a->next)
+    for (cl_node_t* b = a; b != l->head && greater_than(b->prev,b); b = b->prev)
+      cl_swap_left(l, b);
+}
 
-// // Set the node as the head and tail of the list. This does not check if list
-// // is empty. If used improperly, data will be lost, memory potentially leaked.
-// void cl_set_only_node(cl_node_t* const n, cl_t* const l) {
-//   l->head = n;
-//   l->tail = n;
-//   l->count = 1;
-//   n->prev = n->next = NULL;
-// }
+// Find a node in the list which matches the predicate. If predicate returns 0,
+// no match; for return != 0, it is match.
+cl_node_t* cl_find(int(*predicate)(const cl_node_t*), const cl_t* const l) {
+  for (const cl_node_t* n = l->head; n != NULL; n = n->next)
+    if (predicate(n))
+      return n;
 
-// // Push a node to the front. If the list is empty, node will become head and
-// // tail.
-// void cl_push_front(cl_node_t* const n, cl_t* const l) {
-//   if (is_empty(l)) return set_only_node(n, l);
-  
-//   l->head->prev = n;
-//   n->next = l->head;
-//   n->prev = NULL; // enforce no previous for head node
-//   l->head = n;
-//   l->count++;
-// }
-
-// // Find a node in the list which matches the predicate. If predicate returns 0,
-// // no match; for return != 0, it is match.
-// cl_node_t* cl_find(int(*predicate)(const cl_node_t*), const cl_t* const l) {
-//   for (const cl_node_t* n = l->head; n != NULL; n = n->next)
-//     if (predicate(n)) return n;
-// }
-
-// void cl_rm_node(const cl_node_t* const n, cl_t* const l) {
-//   if (l->head == n){
-//     if (l->tail == n) l->head = l->tail = NULL;
-//     else l->head = n->next;
-//   }
-//   if (n->prev && n->next) { // not front or end
-//       n->next->prev = n->prev;
-//       n->prev->next = n->next;
-//   }
-
-//   if (n->next && !n->prev) {
-//     n->next->prev = NULL;
-//   }
-// }
-
-// void cl_push_back(cl_node_t* n, cl_t* l) {
-//   if (is_empty(l)) return set_only_node(n, l);
-
-//   cl_node_t* const tail_node = l->tail;
-
-//   tail_node->next = n;
-//   n->prev = tail_node;
-//   l->tail = n;
-// }
-
-// // Based on a predicate which returns non-zero to indicate a node should be 
-// // removed, removes the applicable nodes and returns how many were removed;
-// int cl_remove_no_dealloc(cl_t* l, int (*match)(const cl_node_t* const)) {
-//   int num_removed = 0;
-
-//   for (cl_node_t* node = l->head; node != NULL; node = node->next) {
-//     if (match(node)) {
-//       node->next->prev = node->prev;
-//       node->prev = node->next;
-//       node->prev = node->next = NULL;
-//       free(node->data);
-//       node->data = NULL;
-//       free(node);
-//       node = NULL;
-
-//     }
-//   }
-
-//   return num_removed;
-// }
+  return NULL;
+}
 
 // int cl_remove(cl_t* l, int (*predicate)(const cl_node_t* const)) { return 0; }
 
--- tests/clist_unittests.cc
+++ tests/clist_unittests.cc
@@ -4,25 +4,130 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-TEST(cl, initialization) {
+TEST(clist, initialize_list) {
   ASSERT_EQ(0,0);
   cl_t* l = cl_alloc_list();
   ASSERT_EQ(0, cl_size(l));
-  // ASSERT_EQ(NULL, cl_head(l));
-  // ASSERT_EQ(NULL, cl_tail(l));
+  ASSERT_EQ(NULL, cl_head(l));
+  ASSERT_EQ(NULL, cl_tail(l));
+  cl_destroy(l);
 }
 
-// TEST(cl_node, initialization) {
-//   char fifteen_chars[] = "123456789abcde";
-//   size_t count = 1 + strlen(fifteen_chars);
-//   char* buffer = (char*)malloc(sizeof(char) * count);
-//   strcpy(buffer, fifteen_chars);
+TEST(clist, initialize_node) {
+  char fifteen_chars[] = "123456789abcde";
+  size_t num_chars = sizeof(fifteen_chars) / sizeof(fifteen_chars[0]);
+  char* buffer = (char*)malloc(sizeof(*buffer) * num_chars);
+  strcpy(buffer, fifteen_chars);
 
-//   cl_node_t* n = cl_alloc_node(buffer);
-//   ASSERT_EQ(NULL, cl_next(n));
-//   ASSERT_EQ(NULL, cl_prev(n));
+  cl_node_t* n = cl_alloc_node(buffer);
+  ASSERT_EQ(NULL, cl_next(n));
+  ASSERT_EQ(NULL, cl_prev(n));
   
-//   for (int i = 0; i < 15; ++i)
-//     ASSERT_EQ(fifteen_chars[i], ((char*)cl_data(n))[i]);
-// }
+  for (int i = 0; i < 15; ++i)
+    ASSERT_EQ(fifteen_chars[i], ((char*)cl_data(n))[i]);
+
+  cl_destroy_node(n);
+}
+
+TEST(clist, push_pop_one) {
+  {
+    cl_node_t* n = cl_alloc_node(strdup("hello, world"));
+    cl_t* l = cl_alloc_list();
+    cl_push_front(l, n);
+    ASSERT_EQ(1, cl_size(l));
+    ASSERT_EQ(n, cl_head(l));
+    ASSERT_EQ(n, cl_tail(l));
+    cl_destroy(l);
+  }
+  {
+    cl_node_t* n = cl_alloc_node(strdup("hello, world"));
+    cl_t* l = cl_alloc_list();
+    cl_push_back(l, n);
+    ASSERT_EQ(1, cl_size(l));
+    ASSERT_EQ(n, cl_head(l));
+    ASSERT_EQ(n, cl_tail(l));
+    cl_destroy(l);
+  }
+}
+
+TEST(clist, push_pop) {
+  cl_node_t* n1 = cl_alloc_node(strdup("hello, world1"));
+  cl_node_t* n2 = cl_alloc_node(strdup("hello, world2"));
+  cl_node_t* n3 = cl_alloc_node(strdup("hello, world3"));
+  cl_node_t* n4 = cl_alloc_node(strdup("hello, world4"));
+  cl_node_t* n5 = cl_alloc_node(strdup("hello, world5"));
+  cl_t* l = cl_alloc_list();
+
+  cl_push_front(l, n2);
+  cl_push_back (l, n3);
+  ASSERT_EQ(2, cl_size(l));
+  ASSERT_EQ(n2, cl_head(l));
+  ASSERT_EQ(n3, cl_tail(l));
+  
+  cl_push_front(l, n1);
+  ASSERT_EQ(3,  cl_size(l));
+  ASSERT_EQ(n1, cl_head(l));
+  ASSERT_EQ(n3, cl_tail(l));
+  ASSERT_EQ(NULL, cl_prev(n1));
+  ASSERT_EQ(n2,   cl_next(n1));
+  ASSERT_EQ(n1,   cl_prev(n2));
+  ASSERT_EQ(n3,   cl_next(n2));
+  ASSERT_EQ(n2,   cl_prev(n3));
+  ASSERT_EQ(NULL, cl_next(n3));
+
+  cl_push_back(l, n4);
+  ASSERT_EQ(4, cl_size(l));
+  ASSERT_EQ(n1, cl_head(l));
+  ASSERT_EQ(n4, cl_tail(l));
+
+  cl_push_back(l, n5);
+  ASSERT_EQ(5, cl_size(l));
+  ASSERT_EQ(n1, cl_head(l));
+  ASSERT_EQ(n5, cl_tail(l));
+  
+  cl_node_t* nodes[] = { n1, n2, n3, n4, n5 };
+  const size_t num_worlds = sizeof(nodes) / sizeof(nodes[0]);
+
+  for (size_t i = 0; i < num_worlds - 1; ++i) {
+    ASSERT_EQ(nodes[i+1], cl_next(nodes[i]));
+  }
+  for (size_t i = num_worlds; i < 1; ++i) {
+    ASSERT_EQ(nodes[i-1], cl_prev(nodes[i]));
+  }
+
+  char test_str[] = "hello, worldx";
+  const size_t world_idx = 12;
+  for (size_t i = 0; i < num_worlds; ++i) {
+    test_str[world_idx] = (char)('1' + i);
+    ASSERT_STREQ(test_str, (char*)cl_data(nodes[i]));
+  }
+
+  cl_destroy(l);
+}
+
+int sort_int_nodes(cl_node_t* first, cl_node_t* second) {
+  int a = *(int*)cl_data(first);
+  int b = *(int*)cl_data(second);
+  return a == b ? 0
+    : a > b ? 1 : -1;
+}
+
+TEST(clist, sort) {
+  srand(time(NULL));
+  const size_t num_nodes = 100;
+  cl_t* l = cl_alloc_list();
+
+  for (size_t i = 0; i < num_nodes; ++i) {
+    int* num = (int*)malloc(sizeof(*num));
+    *num = rand();
+    cl_push_front(l, cl_alloc_node(num));
+  }
+
+  cl_sort(l, sort_int_nodes);
+
+  for (cl_node_t* n = cl_head(l); n != cl_tail(l); n = cl_next(n)) {
+    
+  }
+}
